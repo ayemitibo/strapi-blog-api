928a7fc0d5ffdb0a4ca032cd9dd791a3b4b026aa
ayemitibo <ayemitiboabiodun02@gmail.com>
2020-10-15 12:39:07 +0100

    chore : created the post controller on strapi admin

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd2542355248e4140c9924f569a346aac'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '473bca47-027e-491c-8ebf-644945bca63f'
+};
